d6d7e8aa2e352eafe3bbfbd8f85a6a38175bc376
amitkocher1314 <amitkocher7206251314@gmail.com>
2024-07-07 00:58:25 +0530

    setting-up-cart-project-full-functonal

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -2,15 +2,24 @@ import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
 const Products = (props) => {
+  const dummy_products = [
+    { id:'p1' , price:6 , title: 'my first book' ,description:'This is a first product - amazing!'},
+    { id:'p2' , price:63, title: 'my second book' ,description:'This is a first product - amazing!'}
+  ]
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {dummy_products.map((product)=>(
+               <ProductItem
+               key = {product.id}
+               id = {product.id}
+               title={product.title}
+               price={product.price}
+               description={product.description}
+             />
+        ))}
+       
       </ul>
     </section>
   );
